6ae61352e47bd560062b11839637772801664897
Alexander Sbitnev <alexander.sbitnev@yandex.ru>
2024-10-11 12:37:48 +0300

    Test bot - updates

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-import aiogram
+import requests
+import time
+
+
+API_URL = 'https://api.telegram.org/bot'
+BOT_TOKEN = '6986373519:AAGkd_dmH4JdMhtjOfm75CuyBmDvSiBMfJQ'
+TEXT = 'Опа, апдейт'
+MAX_COUNTER = 100
+
+offset = -2
+counter = 0
+chat_id: int
+
+
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+    time.sleep(1)
+    counter += 1
