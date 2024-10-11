5f1cd26bc56bda0493005b2c19ee6fb25a241d7c
Alexander Sbitnev <alexander.sbitnev@yandex.ru>
2024-10-11 18:10:38 +0300

    Test bot - kotobot User name showing

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from asyncio import timeout
+
 import requests
 # from pprint import pprint
 import time
@@ -12,13 +14,14 @@ ERROR_TEXT = 'Здесь должна была быть картинка с ко
 
 offset = -2
 counter = 0
+time_out = 50
 cat_response: requests.Response
 cat_link: str
 
 
 while True:
-    print('attempt =', counter)
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+    # print('attempt =', counter)
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}&timeout={time_out}').json()
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
@@ -27,6 +30,7 @@ while True:
             if cat_response.status_code == 200:
                 cat_link = cat_response.json()[0]['url']
                 requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+                print(f"A cat was sent to user {result['message']['from']['first_name']}")
             else:
                 requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
 
